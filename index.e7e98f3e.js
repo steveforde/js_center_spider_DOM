b5948031a6a090a81776989ee2cc3d38222d11a9
Stephen Forde <sforde08@gmail.com>
2023-12-22 16:20:31 +0000

    make build

---
+++ index.e7e98f3e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,c=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.position="relative",t.style.top="".concat(c/2-n/2,"px"),t.style.left="".concat(l/2-i/2,"px");
+//# sourceMappingURL=index.e7e98f3e.js.map
